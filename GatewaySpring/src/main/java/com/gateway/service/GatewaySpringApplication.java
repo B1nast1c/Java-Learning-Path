a5960c2e88138a53a1e81bc094cd77a0b6d51ca3
valnaevis <vgoyzuetat@ulasalle.edu.pe>
2024-04-29 18:03:09 -0500

    Gateway FINAL

--- GatewaySpring/src/main/java/com/gateway/service/GatewaySpringApplication.java
+++ GatewaySpring/src/main/java/com/gateway/service/GatewaySpringApplication.java
@@ -2,12 +2,15 @@ package com.gateway.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewaySpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewaySpringApplication.class, args);
     }
-
 }
--- GatewaySpring/src/test/java/com/gateway/service/GatewaySpringApplicationTests.java
+++ GatewaySpring/src/test/java/com/gateway/service/GatewaySpringApplicationTests.java
@@ -1,12 +1,7 @@
 package com.gateway.service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GatewaySpringApplicationTests {
-
-    @Test
     void contextLoads() {
     }
 
